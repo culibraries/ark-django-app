ae3b52f32f462f4cbfed7d1dd154b4085841840d
Mark Stacy <mbstacy@gmail.com>
2020-08-14 14:06:16 -0600

    cleanup and env variable for ARK_CATALOG_COLLECTION

--- views.py
+++ views.py
@@ -4,28 +4,25 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.renderers import TemplateHTMLRenderer
 import json
 import os
-import requests
-import base64
+# import requests
+# import base64
+import arkpy
 from .permission import arkPermission
 from itertools import groupby
 from django.http import HttpResponseRedirect
-from django.template import loader
+from catalog.views import Catalog, CatalogData, CatalogDataDetail
 
-cybercom_token = os.getenv('CYBERCOM_TOKEN')
-cybercom_catalog_url = os.getenv(
-    'ARK_CATALOG_URL', "/api/catalog/data/catalog/ark/")
-cybercom_ark_collection='ark'
-from catalog.views import Catalog, CatalogData,CatalogDataDetail 
+cybercom_ark_collection = os.getenv('ARK_CATALOG_COLLECTION', 'ark')
 
-class arkAcknowledgement
+
+class arkAcknowledgement(APIView):
     """
     A view that returns CU Boulder Acknowledgement.
     """
     renderer_classes = [TemplateHTMLRenderer]
 
-    def get(self, request,naan=None, format=None):
+    def get(self, request, naan=None, format=None):
         return Response({}, template_name='arkAcknowledgement.html.js')
-
 
 
 class ArkServer(APIView):
@@ -37,36 +34,32 @@ class ArkServer(APIView):
         if not naan:
             # return list of arks
             #request.GET['collection'] = cybercom_ark_collection
-            return CatalogData.get(request,database='Catalog',collection=cybercom_ark_collection,format='json')
+            return CatalogData.get(request, database='Catalog', collection=cybercom_ark_collection, format='json')
         elif naan and ark:
             # check for ? ?? or ??? if no question marks resolve
-            url=request.build_absolute_uri()
-            groups=groupby(url)
-            result=[(label, sum(1 for _ in group)) for label, group in groups if label=='?']
-            query = '{"format":{"ark":"' + naan + '/' +  ark + '"}'
-            request.GET['query'] = query
-            data=CatalogData.get(request,database='Catalog',collection=cybercom_ark_collection,format='json')
-            item= json.loads(data['results'][0])
+            url = request.build_absolute_uri()
+            groups = groupby(url)
+            result = [(label, sum(1 for _ in group))
+                      for label, group in groups if label == '?']
+            # Pull Record
+            data = self.pullRecord(request, naan, ark)
+            item = json.loads(data['results'][0])
             if not result:
-                #Resolve
-                return HttpResponseRedirect(item["resolve_url"]) 
+                # Resolve
+                return HttpResponseRedirect(item["resolve_url"])
             elif result[0][1] == 1:
-                #min response
+                # min response
                 return Response(item)
             elif result[0][1] == 2:
-                #expanded response
+                # expanded response
                 return Response(item)
-            elif result[0][1] => 3:
-            # check for ? ?? or ??? if no question marks resolve
+            elif result[0][1] = > 3:
+                # check for ? ?? or ??? if no question marks resolve
                 return Response(item)
         else:
-            data= {"error":"Error occured both NAAN and ARK are required for GET operations. Please "}
+            data = {
+                "error": "Error occured both NAAN and ARK are required for GET operations. Please "}
             return Response(data)
-
-
-        # response = redirect(cybercom_catalog_url)
-        # return response
-        # return True
 
     def post(self, request, naan=None, ark=None, format=None):
         """
@@ -90,12 +83,21 @@ class ArkServer(APIView):
         # Set Metadata
         arkMeta = self.registerARK(ark, request.data)
         # Store Metadata
-        headers = {"Content-Type": "application/json",
-                   "Authorization": "Token {0}".format(cybercom_token)}
+        self.saveCatlog(request)
 
         return ark
-    def saveCatlog(self, request,data):
-        data=CatalogData.post(request,database='Catalog',collection=cybercom_ark_collection,format='json')
+
+    def pullRecord(self, request, naan, ark):
+        query = '{"format":{"ark":"' + naan + '/' + ark + '"}'
+        request.GET['query'] = query
+        data = CatalogData.get(request, database='Catalog',
+                               collection=cybercom_ark_collection, format='json')
+
+        return data
+
+    def saveCatlog(self, request):
+        data = CatalogData.post(
+            request, database='Catalog', collection=cybercom_ark_collection, format='json')
 
     def mint(self, naan, template, prefix):
         ark = arkpy.mint(naan, template, prefix)
@@ -110,17 +112,14 @@ class ArkServer(APIView):
         if not arkpy.validate(ark):
             return False
         # Catalog URL
-        base_url = "/".join(request.build_absolute_url.split('/')[:3])
-        query = '{"filter":{"ark":"' + ark + '"}'
-        url = "{0}{1}?query={2}&format=json".format(
-            base_url, cybercom_catalog_url, query)
-        req = requests.get(url)
-        data = req.json()
+        naan, ark = ark.split('/')
+        data = json.loads(self.pullRecord(request, naan, ark))
         if data['count'] > 0:
             return False
         return True
 
     def registerARK(self, ark, data):
-        temp = {"ark": ark, "resolve_url": "", "retired_url": [],"metadata":{}}
+        temp = {"ark": ark, "resolve_url": "",
+                "retired_url": [], "metadata": {}}
         data['ark'] = ark
         return temp.update(data)
