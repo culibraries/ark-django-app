55690d4b28b2f3a9109796caa365c898bfe02849
Mark Stacy <mbstacy@gmail.com>
2020-08-15 13:28:28 -0600

    update urls

--- urls.py
+++ urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('', ArkServer.as_view(), name='ark-list'),
     path('<naan>/',
          arkAcknowledgement.as_view(), name='ark-statement'),
-    path('<naan>/<ark>/',
+    path('<naan>/<ark>',
          ArkServer.as_view(), name='ark-detail'),
 
 ]
