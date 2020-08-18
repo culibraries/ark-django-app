57bf335aa53718a58b73df32554bceb74b1858e1
Mark Stacy <mbstacy@gmail.com>
2020-08-17 23:17:14 -0600

    regex pattern

--- urls.py
+++ urls.py
@@ -5,6 +5,6 @@ from django.urls import include, path, re_path
 urlpatterns = [
     path('', ArkServer.as_view(), name='ark-list'),
     path('<naan>/', arkAcknowledgement.as_view(), name='ark-statement'),
-    path('<naan>/<ark>', ArkServer.as_view(), name='ark-detail'),
+    re_path(r'<naan>/<ark>/?$', ArkServer.as_view(), name='ark-detail'),
     path('<naan>/<ark>/detail', ArkDetail.as_view(), name='ark-details'),
 ]
